5b4eadfd66dea48d8e20625a93ad0b03a42116d1
Matsuda Kazuhiko <mkazuhik@c5r2s9.42tokyo.jp>
2025-08-09 00:25:58 +0900

    a

--- src/pipex.c
+++ src/pipex.c
@@ -3,42 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkazuhik <mkazuhik@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: gcollet <gcollet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/08 15:34:04 by mkazuhik          #+#    #+#             */
-/*   Updated: 2025/08/08 17:23:53 by mkazuhik         ###   ########.fr       */
+/*   Created: 2021/08/02 09:54:02 by gcollet           #+#    #+#             */
+/*   Updated: 2022/02/06 13:47:21 by gcollet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "../includes/pipex.h"
 
-// int	main(int argc, char **argv)
-// {
-// 	if (argc != 5)
-// 	{
-// 		ft_putstr_fd("Usage: ./pipex infile cmd1 cmd2 outfile\n", STDERR_FILENO);
-// 		return (EXIT_FAILURE);
-// 	}
+/* Child process that run inside a fork, take the filein, put the output inside
+ a pipe and then close with the exec function */
+void	child_process(char **argv, char **envp, int *fd)
+{
+	int		filein;
 
-// 	int fd_in = open(argv[1], O_RDONLY);
-// 	if (fd_in < 0)
-// 	{
-// 		perror("Error opening input file");
-// 		return (EXIT_FAILURE);
-// 	}
+	filein = open(argv[1], O_RDONLY, 0777);
+	if (filein == -1)
+		error();
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(filein, STDIN_FILENO);
+	close(fd[0]);
+	execute(argv[2], envp);
+}
 
-// 	int fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-// 	if (fd_out < 0)
-// 	{
-// 		perror("Error opening output file");
-// 		close(fd_in);
-// 		return (EXIT_FAILURE);
-// 	}
+/* Parent process that take the data from the pipe, change the output for the
+ fileout and also close with the exec function */
+void	parent_process(char **argv, char **envp, int *fd)
+{
+	int		fileout;
 
-// 	// Here you would typically set up pipes and fork processes to execute cmd1 and cmd2
-// 	// For simplicity, this example does not include the full implementation.
+	fileout = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fileout == -1)
+		error();
+	dup2(fd[0], STDIN_FILENO);
+	dup2(fileout, STDOUT_FILENO);
+	close(fd[1]);
+	execute(argv[3], envp);
+}
 
-// 	close(fd_in);
-// 	close(fd_out);
-// 	return (EXIT_SUCCESS);
-// }
+/* Main function that run the child and parent process or display an error
+ message if arguments are wrong */
+int	main(int argc, char **argv, char **envp)
+{
+	int		fd[2];
+	pid_t	pid1;
+
+	if (argc == 5)
+	{
+		if (pipe(fd) == -1)
+			error();
+		pid1 = fork();
+		if (pid1 == -1)
+			error();
+		if (pid1 == 0)
+			child_process(argv, envp, fd);
+		waitpid(pid1, NULL, 0);
+		parent_process(argv, envp, fd);
+	}
+	else
+	{
+		ft_putstr_fd("\033[31mError: Bad arguments\n\e[0m", 2);
+		ft_putstr_fd("Ex: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 1);
+	}
+	return (0);
+}
