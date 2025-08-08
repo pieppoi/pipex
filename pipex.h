a62fa0990a92bb6ee5e67c803144c376766591bf
Matsuda Kazuhiko <mkazuhik@c5r2s9.42tokyo.jp>
2025-08-08 17:25:27 +0900

    a

--- pipex.h
+++ pipex.h
@@ -6,14 +6,13 @@
 /*   By: mkazuhik <mkazuhik@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 14:12:30 by mkazuhik          #+#    #+#             */
-/*   Updated: 2025/08/08 16:23:57 by mkazuhik         ###   ########.fr       */
+/*   Updated: 2025/08/08 17:23:13 by mkazuhik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "ft_printf/ft_printf.h"
 # include <unistd.h>     // read, write, access, close, unlink, dup, dup2, execve, fork, pipe
 # include <fcntl.h>      // open
 # include <stdlib.h>     // malloc, free, exit
@@ -21,5 +20,6 @@
 # include <string.h>     // strerror
 # include <sys/wait.h>   // wait, waitpid
 
+void	ft_error(const char *msg);
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,39 +6,39 @@
 /*   By: mkazuhik <mkazuhik@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 15:34:04 by mkazuhik          #+#    #+#             */
-/*   Updated: 2025/08/08 16:28:38 by mkazuhik         ###   ########.fr       */
+/*   Updated: 2025/08/08 17:23:53 by mkazuhik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
-{
-	if (argc != 5)
-	{
-		ft_putstr_fd("Usage: ./pipex infile cmd1 cmd2 outfile\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 5)
+// 	{
+// 		ft_putstr_fd("Usage: ./pipex infile cmd1 cmd2 outfile\n", STDERR_FILENO);
+// 		return (EXIT_FAILURE);
+// 	}
 
-	int fd_in = open(argv[1], O_RDONLY);
-	if (fd_in < 0)
-	{
-		perror("Error opening input file");
-		return (EXIT_FAILURE);
-	}
+// 	int fd_in = open(argv[1], O_RDONLY);
+// 	if (fd_in < 0)
+// 	{
+// 		perror("Error opening input file");
+// 		return (EXIT_FAILURE);
+// 	}
 
-	int fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_out < 0)
-	{
-		perror("Error opening output file");
-		close(fd_in);
-		return (EXIT_FAILURE);
-	}
+// 	int fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 	if (fd_out < 0)
+// 	{
+// 		perror("Error opening output file");
+// 		close(fd_in);
+// 		return (EXIT_FAILURE);
+// 	}
 
-	// Here you would typically set up pipes and fork processes to execute cmd1 and cmd2
-	// For simplicity, this example does not include the full implementation.
+// 	// Here you would typically set up pipes and fork processes to execute cmd1 and cmd2
+// 	// For simplicity, this example does not include the full implementation.
 
-	close(fd_in);
-	close(fd_out);
-	return (EXIT_SUCCESS);
-}
+// 	close(fd_in);
+// 	close(fd_out);
+// 	return (EXIT_SUCCESS);
+// }
